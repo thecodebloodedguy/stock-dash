1ae94839a696ae301e7ebaca9f79809ab7bcbbf4
Bishal Sahoo <thecodebloodedguy@protonmail.ch>
2023-01-12 03:52:41 +0530

    modified:   app.py 	deleted:    assests/Best-Stocks.jpeg 	deleted:    assests/Stonks_emoji.svg 	deleted:    assests/stylesheet.css

--- app.py
+++ app.py
@@ -6,14 +6,18 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import base64
+import yfinance as yf
+import pandas as pd
+import plotly.graph_objs as go
+import plotly.express as px
 app=dash.Dash(__name__)
 server=app.server
-imgfile=r'\assests\Best-Stocks.jpeg'
+imgfile=r'D:\Work\Dash Stock App\assets\Best-Stocks.jpeg'
 with open(imgfile, "rb") as image_file:
     img_data = base64.b64encode(image_file.read())
     img_data = img_data.decode()
     logo_img = "{}{}".format("data:image/jpg;base64, ", img_data)
-app.head = [html.Link(rel='stylesheet', href='assets/stylesheet.css')]
+app.head = html.Link(rel='stylesheet', href='assets/stylesheet.css')
 app.layout=html.Div(children=
     [
         html.Div(children=
@@ -23,7 +27,7 @@ app.layout=html.Div(children=
               #Stock Code
               html.Label("Stock Code:"),
               dcc.Input(id='code',type='text'),
-              html.Button('Submit',id='sub',value='submit')
+              html.Button('Submit',id='sub',value='submit',className='btn')
 
             ]),
             html.Div(children=[
@@ -35,24 +39,24 @@ app.layout=html.Div(children=
             ]),
             html.Div(children=[
               # Stock price button
-              html.Button('Stock Price',id='stp'),
+              html.Button('Stock Price',id='stp',className='btn'),
               # Indicators button
-              html.Button('Indicators',id='sti'),
+              html.Button('Indicators',id='sti',className='btn'),
               # Number of days of forecast input
               html.Br(),
               dcc.Input('Number of Days',type='text',id='stn'),
               # Forecast button
-              html.Button('Forecast',id='stf')
+              html.Button('Forecast',id='stf',className='btn')
 
             ]),
           ],
-        className="nav"),
+        className="nav child"),
 
         html.Div(children=
           [
             html.Div(children=
                   [  # Logo
-                    html.Img(src=logo_img,id='logo'),
+                    html.Img(src=logo_img,id='logo',width="50px",height="50px"),
                     # Company Name
                     html.H1('Stonks Market',id='c_name')
                   ],
@@ -69,8 +73,8 @@ app.layout=html.Div(children=
                 # Forecast plot
             ], id="forecast-content")
           ],
-        className="content")
-    ]
+        className="content child")
+    ], className="parent"
 )
 
 if __name__ == '__main__':
